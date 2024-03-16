4c0aec8f84f2f7487ddca9ca5ec35405ef6854f3
Kir4R00t <miki4sa@interia.pl>
2024-03-16 20:40:57 +0100

    fixing minor inconveniences

---
+++ assets/js/typing.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['Python', 'Django', 'C++', 'PHP', 'SQL', 'HTML', 'CSS', 'JavaScript'],
+    typeSpeed: 175,
+    backSpeed: 100,
+    loop: true,
+    });
