2e04a10d6ea855e8c7ef29a4a3cb5105a20cd775
Kir4R00t <miki4sa@interia.pl>
2024-03-31 21:47:14 +0200

    adding stuff

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,6 +1,6 @@
 // Very cool typing animation
 var typed = new Typed('#element', {
-    strings: ['Python', 'Django', 'C++', 'PHP', 'SQL', 'HTML', 'CSS', 'JavaScript'],
+    strings: ['Java', 'Python', 'Django', 'C++', 'PHP', 'SQL', 'HTML', 'CSS', 'JavaScript'],
     typeSpeed: 175,
     backSpeed: 100,
     loop: true,
