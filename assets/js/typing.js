30a8ca9e5b47ccd4d0766dce01c1b1740d6ccffa
Kir4R00t <miki4sa@interia.pl>
2024-11-13 16:58:19 +0100

    updating 'my skills' & other minor changes

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,6 +1,6 @@
 // Very cool typing animation
 var typed = new Typed('#element', {
-    strings: ['Java', 'Python', 'Django', 'C++', 'PHP', 'SQL', 'HTML', 'CSS', 'JavaScript'],
+    strings: ['Java', 'Python', 'C++', 'PHP', 'SQL', 'HTML', 'CSS', 'JavaScript'],
     typeSpeed: 175,
     backSpeed: 100,
     loop: true,
