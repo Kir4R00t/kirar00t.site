c8918a6f00276b715bc6f546a507aa6975ee0466
Kir4R00t-thinkpad <55001500+Kir4R00t@users.noreply.github.com>
2026-03-26 09:34:53 +0100

    website update

--- assets/js/typing.js
+++
@@ -1,7 +0,0 @@
-// Very cool typing animation
-var typed = new Typed('#element', {
-    strings: ['Python', 'Docker', 'Linux', 'Git', 'REST', 'QtQML', 'JSON', 'HTML', 'CSS'],
-    typeSpeed: 175,
-    backSpeed: 100,
-    loop: true,
-    });
